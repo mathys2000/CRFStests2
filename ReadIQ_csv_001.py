69fb1c611a5166e4c42e98a9633289c6d7725243
viby <mathys@colorado.edu>
2026-06-30 12:35:05 -0600

    updated README file

--- ReadIQ_csv_001.py
+++ ReadIQ_csv_001.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.19.4
+#       jupytext_version: 1.19.3
 #   kernelspec:
 #     display_name: polar [conda env:polar]
 #     language: python
